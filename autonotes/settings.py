ed4c56907feba5b183953baa0a4db8ae13ae99c7
crocodilered <serge.zolotukhin@gmail.com>
2020-04-18 22:08:18 +0300

    Remove browsable api interface.

--- autonotes/settings.py
+++ autonotes/settings.py
@@ -82,6 +82,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 SIMPLE_JWT = {
