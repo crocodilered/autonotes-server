a9a6ecb821c05e2d212eabb0603d544d50712daa
crocodilered <serge.zolotukhin@gmail.com>
2020-04-18 17:53:55 +0300

    Update DEFAULT_PERMISSION_CLASSES

--- autonotes/settings.py
+++ autonotes/settings.py
@@ -74,10 +74,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
